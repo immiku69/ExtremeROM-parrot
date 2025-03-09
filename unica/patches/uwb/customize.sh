63dfc797ae3585685f326bd62f8e0530787868bd
Ocin4ever <85343782+Ocin4ever@users.noreply.github.com>
2025-03-09 17:13:39 +0100

    unica: disable UWB on unsupported devices (#7)

--- unica/patches/uwb/customize.sh
+++ unica/patches/uwb/customize.sh
@@ -1,42 +1,46 @@
 SOURCE_FIRMWARE_PATH="$FW_DIR/$(echo -n "$SOURCE_FIRMWARE" | sed 's./._.g' | rev | cut -d "_" -f2- | rev)"
 TARGET_FIRMWARE_PATH="$FW_DIR/$(echo -n "$TARGET_FIRMWARE" | sed 's./._.g' | rev | cut -d "_" -f2- | rev)"
 
-if [ -f "$TARGET_FIRMWARE_PATH/system/system/etc/libuwb-cal.conf" ]; then
-    if ! grep -q "uwbcountrycode" "$WORK_DIR/product/etc/build.prop"; then
-        sed -i "/usb.config/a ro.boot.uwbcountrycode=ff" "$WORK_DIR/product/etc/build.prop"
-    fi
+# [
+REMOVE_FROM_WORK_DIR()
+{
+    local FILE_PATH="$1"
 
-    ADD_TO_WORK_DIR "$TARGET_FIRMWARE_PATH" "system" "system/etc/libuwb-cal.conf" 0 0 644 "u:object_r:system_file:s0"
-    ADD_TO_WORK_DIR "$TARGET_FIRMWARE_PATH" "system" "system/etc/pp_model.tflite" 0 0 644 "u:object_r:system_file:s0"
+    if [ -e "$FILE_PATH" ]; then
+        local FILE
+        local PARTITION
+        FILE="$(echo -n "$FILE_PATH" | sed "s.$WORK_DIR/..")"
+        PARTITION="$(echo -n "$FILE" | cut -d "/" -f 1)"
 
-    if [ ! -d "$SOURCE_FIRMWARE_PATH/system/system/app/UwbUci" ]; then
-        ADD_TO_WORK_DIR "$SRC_DIR/prebuilts/dm3qxxx" "system" \
-            "system/app/UwbUci/UwbUci.apk" 0 0 644 "u:object_r:system_file:s0"
-        ADD_TO_WORK_DIR "$SRC_DIR/prebuilts/dm3qxxx" "system" \
-            "system/etc/init/init.system.uwb.rc" 0 0 644 "u:object_r:system_file:s0"
-        ADD_TO_WORK_DIR "$SRC_DIR/prebuilts/dm3qxxx" "system" \
-            "system/etc/permissions/com.samsung.android.uwb_extras.xml" 0 0 644 "u:object_r:system_file:s0"
-        ADD_TO_WORK_DIR "$SRC_DIR/prebuilts/dm3qxxx" "system" \
-            "system/etc/permissions/org.carconnectivity.android.digitalkey.timesync.xml" 0 0 644 "u:object_r:system_file:s0"
-        ADD_TO_WORK_DIR "$SRC_DIR/prebuilts/dm3qxxx" "system" \
-            "system/etc/permissions/privapp-permissions-com.samsung.android.dcktimesync.xml" 0 0 644 "u:object_r:system_file:s0"
-        ADD_TO_WORK_DIR "$SRC_DIR/prebuilts/dm3qxxx" "system" \
-            "system/etc/permissions/samsung.uwb.xml" 0 0 644 "u:object_r:system_file:s0"
-        ADD_TO_WORK_DIR "$SRC_DIR/prebuilts/dm3qxxx" "system" \
-            "system/framework/com.samsung.android.uwb_extras.jar" 0 0 644 "u:object_r:system_file:s0"
-        ADD_TO_WORK_DIR "$SRC_DIR/prebuilts/dm3qxxx" "system" \
-            "system/framework/samsung.uwb.jar" 0 0 644 "u:object_r:system_file:s0"
-        ADD_TO_WORK_DIR "$SRC_DIR/prebuilts/dm3qxxx" "system" \
-            "system/lib/libtflite_uwb_jni.so" 0 0 644 "u:object_r:system_lib_file:s0"
-        ADD_TO_WORK_DIR "$SRC_DIR/prebuilts/dm3qxxx" "system" \
-            "system/lib/libuwb_uci_jni_rust.so" 0 0 644 "u:object_r:system_lib_file:s0"
-        ADD_TO_WORK_DIR "$SRC_DIR/prebuilts/dm3qxxx" "system" \
-            "system/lib64/libtflite_uwb_jni.so" 0 0 644 "u:object_r:system_lib_file:s0"
-        ADD_TO_WORK_DIR "$SRC_DIR/prebuilts/dm3qxxx" "system" \
-            "system/lib64/libuwb_uci_jni_rust.so" 0 0 644 "u:object_r:system_lib_file:s0"
-        ADD_TO_WORK_DIR "$SRC_DIR/prebuilts/dm3qxxx" "system_ext" \
-            "framework/org.carconnectivity.android.digitalkey.timesync.jar" 0 0 644 "u:object_r:system_file:s0"
-        ADD_TO_WORK_DIR "$SRC_DIR/prebuilts/dm3qxxx" "system_ext" \
-            "priv-app/DckTimeSyncService/DckTimeSyncService.apk" 0 0 644 "u:object_r:system_file:s0"
+        echo "Debloating /$FILE"
+        rm -rf "$FILE_PATH"
+
+        [[ "$PARTITION" == "system" ]] && FILE="$(echo "$FILE" | sed 's.^system/system/.system/.')"
+        FILE="$(echo -n "$FILE" | sed 's/\//\\\//g')"
+        sed -i "/$FILE /d" "$WORK_DIR/configs/fs_config-$PARTITION"
+
+        FILE="$(echo -n "$FILE" | sed 's/\./\\\\\./g')"
+        sed -i "/$FILE /d" "$WORK_DIR/configs/file_context-$PARTITION"
     fi
+}
+# ]
+
+MODEL=$(echo -n "$TARGET_FIRMWARE" | cut -d "/" -f 1)
+REGION=$(echo -n "$TARGET_FIRMWARE" | cut -d "/" -f 2)
+
+if [ ! -d "$FW_DIR/${MODEL}_${REGION}/system/system/app/UwbUci" ]; then
+    echo "Removing UWB blobs..."
+    REMOVE_FROM_WORK_DIR "$WORK_DIR/system/system/app/UwbUci"
+    REMOVE_FROM_WORK_DIR "$WORK_DIR/system/system/app/UwbTest"
+    REMOVE_FROM_WORK_DIR "$WORK_DIR/system/system/etc/init/init.system.uwb.rc"
+    REMOVE_FROM_WORK_DIR "$WORK_DIR/system/system/etc/permissions/com.samsung.android.uwb_extras.xml"
+    REMOVE_FROM_WORK_DIR "$WORK_DIR/system/system/etc/permissions/samsung.uwb.xml"
+    REMOVE_FROM_WORK_DIR "$WORK_DIR/system/system/framework/com.samsung.android.uwb_extras.jar"
+    REMOVE_FROM_WORK_DIR "$WORK_DIR/system/system/framework/samsung.uwb.jar"
+    REMOVE_FROM_WORK_DIR "$WORK_DIR/system/system/lib/libtflite_uwb_jni.so"
+    REMOVE_FROM_WORK_DIR "$WORK_DIR/system/system/lib/libuwb_uci_jni_rust.so"
+    REMOVE_FROM_WORK_DIR "$WORK_DIR/system/system/lib64/libtflite_uwb_jni.so"
+    REMOVE_FROM_WORK_DIR "$WORK_DIR/system/system/lib64/libuwb_uci_jni_rust.so"
+else
+    echo "Target has UWB. Ignoring."
 fi
