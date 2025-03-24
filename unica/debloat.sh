9b1c78de0546c1044ce77b5eeac98af29127de88
Yagzie <97476482+Yagzie@users.noreply.github.com>
2025-03-24 16:46:15 +0200

    unica: debloat: add vendor sepolicy version
    Signed-off-by: ExtremeXT <75576145+ExtremeXT@users.noreply.github.com>

--- unica/debloat.sh
+++ unica/debloat.sh
@@ -48,6 +48,11 @@ bin/install-recovery.sh
 etc/init/vendor_flash_recovery.rc
 "
 
+# SEPolicy version
+VENDOR_DEBLOAT+="
+etc/selinux/vendor_sepolicy_version
+"
+
 # Apps debloat
 PRODUCT_DEBLOAT+="
 app/AssistantShell
