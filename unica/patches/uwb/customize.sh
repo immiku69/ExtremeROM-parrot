68cc8da1ab374af45df9a668d6b5c3c1e78787aa
ExtremeXT <75576145+ExtremeXT@users.noreply.github.com>
2025-03-29 13:41:22 +0200

    unica: patches: adapt UWB and SPEN to new buildenv
    Signed-off-by: ExtremeXT <75576145+ExtremeXT@users.noreply.github.com>

--- unica/patches/uwb/customize.sh
+++ unica/patches/uwb/customize.sh
@@ -1,46 +1,19 @@
-SOURCE_FIRMWARE_PATH="$FW_DIR/$(echo -n "$SOURCE_FIRMWARE" | sed 's./._.g' | rev | cut -d "_" -f2- | rev)"
-TARGET_FIRMWARE_PATH="$FW_DIR/$(echo -n "$TARGET_FIRMWARE" | sed 's./._.g' | rev | cut -d "_" -f2- | rev)"
-
-# [
-REMOVE_FROM_WORK_DIR()
-{
-    local FILE_PATH="$1"
-
-    if [ -e "$FILE_PATH" ]; then
-        local FILE
-        local PARTITION
-        FILE="$(echo -n "$FILE_PATH" | sed "s.$WORK_DIR/..")"
-        PARTITION="$(echo -n "$FILE" | cut -d "/" -f 1)"
-
-        echo "Debloating /$FILE"
-        rm -rf "$FILE_PATH"
-
-        [[ "$PARTITION" == "system" ]] && FILE="$(echo "$FILE" | sed 's.^system/system/.system/.')"
-        FILE="$(echo -n "$FILE" | sed 's/\//\\\//g')"
-        sed -i "/$FILE /d" "$WORK_DIR/configs/fs_config-$PARTITION"
-
-        FILE="$(echo -n "$FILE" | sed 's/\./\\\\\./g')"
-        sed -i "/$FILE /d" "$WORK_DIR/configs/file_context-$PARTITION"
-    fi
-}
-# ]
-
 MODEL=$(echo -n "$TARGET_FIRMWARE" | cut -d "/" -f 1)
 REGION=$(echo -n "$TARGET_FIRMWARE" | cut -d "/" -f 2)
 
 if [ ! -d "$FW_DIR/${MODEL}_${REGION}/system/system/app/UwbUci" ]; then
     echo "Removing UWB blobs..."
-    REMOVE_FROM_WORK_DIR "$WORK_DIR/system/system/app/UwbUci"
-    REMOVE_FROM_WORK_DIR "$WORK_DIR/system/system/app/UwbTest"
-    REMOVE_FROM_WORK_DIR "$WORK_DIR/system/system/etc/init/init.system.uwb.rc"
-    REMOVE_FROM_WORK_DIR "$WORK_DIR/system/system/etc/permissions/com.samsung.android.uwb_extras.xml"
-    REMOVE_FROM_WORK_DIR "$WORK_DIR/system/system/etc/permissions/samsung.uwb.xml"
-    REMOVE_FROM_WORK_DIR "$WORK_DIR/system/system/framework/com.samsung.android.uwb_extras.jar"
-    REMOVE_FROM_WORK_DIR "$WORK_DIR/system/system/framework/samsung.uwb.jar"
-    REMOVE_FROM_WORK_DIR "$WORK_DIR/system/system/lib/libtflite_uwb_jni.so"
-    REMOVE_FROM_WORK_DIR "$WORK_DIR/system/system/lib/libuwb_uci_jni_rust.so"
-    REMOVE_FROM_WORK_DIR "$WORK_DIR/system/system/lib64/libtflite_uwb_jni.so"
-    REMOVE_FROM_WORK_DIR "$WORK_DIR/system/system/lib64/libuwb_uci_jni_rust.so"
+    DELETE_FROM_WORK_DIR "system" "system/app/UwbUci"
+    DELETE_FROM_WORK_DIR "system" "system/app/UwbTest"
+    DELETE_FROM_WORK_DIR "system" "system/etc/init/init.system.uwb.rc"
+    DELETE_FROM_WORK_DIR "system" "system/etc/permissions/com.samsung.android.uwb_extras.xml"
+    DELETE_FROM_WORK_DIR "system" "system/etc/permissions/samsung.uwb.xml"
+    DELETE_FROM_WORK_DIR "system" "system/framework/com.samsung.android.uwb_extras.jar"
+    DELETE_FROM_WORK_DIR "system" "system/framework/samsung.uwb.jar"
+    DELETE_FROM_WORK_DIR "system" "system/lib/libtflite_uwb_jni.so"
+    DELETE_FROM_WORK_DIR "system" "system/lib/libuwb_uci_jni_rust.so"
+    DELETE_FROM_WORK_DIR "system" "system/lib64/libtflite_uwb_jni.so"
+    DELETE_FROM_WORK_DIR "system" "system/lib64/libuwb_uci_jni_rust.so"
 else
     echo "Target has UWB. Ignoring."
 fi
