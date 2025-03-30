729e2480fd3cf3c0fbc2be9eb1dc038fd7e6df76
ExtremeXT <75576145+ExtremeXT@users.noreply.github.com>
2025-03-30 16:12:00 +0300

    scripts: blobs: fix model and IMEI AGAIN
    Signed-off-by: ExtremeXT <75576145+ExtremeXT@users.noreply.github.com>

--- scripts/internal/update_prebuilt_blobs.sh
+++ scripts/internal/update_prebuilt_blobs.sh
@@ -67,7 +67,7 @@ case "$1" in
         FIRMWARE="SM-A736B/XME/352828291234563"
         ;;
     "prebuilts/samsung/p3sxxx")
-        FIRMWARE="SM-G998B/AUT/355719111234569"
+        FIRMWARE="SM-G998B/AUT/353769590494467"
         ;;
     "prebuilts/samsung/pa3qzcx")
         FIRMWARE="SM-S9380/CHC/355534491000674"
