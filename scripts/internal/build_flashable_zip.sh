1ff9a35ebd1721f1f9fdf8292e0c059d262777a3
ExtremeXT <75576145+ExtremeXT@users.noreply.github.com>
2025-03-30 21:54:33 +0300

    scripts: fixup zip naming
    Signed-off-by: ExtremeXT <75576145+ExtremeXT@users.noreply.github.com>

--- scripts/internal/build_flashable_zip.sh
+++ scripts/internal/build_flashable_zip.sh
@@ -584,8 +584,8 @@ echo "Generate build_info.txt"
 GENERATE_BUILD_INFO
 
 echo "Creating zip"
-[ -f "$OUT_DIR/rom.zip" ] && rm -f "$OUT_DIR/rom.zip"
-cd "$TMP_DIR" ; zip -rq ../rom.zip ./* ; cd - &> /dev/null
+[ -f "$OUT_DIR/$FILE_NAME.zip" ] && rm -f "$OUT_DIR/$FILE_NAME.zip"
+cd "$TMP_DIR" ; zip -rq ../$FILE_NAME.zip ./* ; cd - &> /dev/null
 
 echo "Deleting tmp dir"
 rm -rf "$TMP_DIR"
