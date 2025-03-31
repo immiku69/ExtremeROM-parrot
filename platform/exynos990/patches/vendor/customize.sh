c6e02c22807b11321c131fb7a29d2e84d20c1fd7
ExtremeXT <75576145+ExtremeXT@users.noreply.github.com>
2025-03-31 21:57:41 +0300

    platform: exynos990: patches: vendor: fixup
    Signed-off-by: ExtremeXT <75576145+ExtremeXT@users.noreply.github.com>

--- platform/exynos990/patches/vendor/customize.sh
+++ platform/exynos990/patches/vendor/customize.sh
@@ -1,26 +1,13 @@
 echo "Updating Vibrator/RIL/Face/WPA HALs..."
+# Delete hermes to get rid of weaver encryption blobs
 BLOBS_LIST="
 bin/hw/vendor.samsung.hardware.vibrator@2.2-service
 etc/init/vendor.samsung.hardware.vibrator@2.2-service.rc
 lib64/vendor.samsung.hardware.vibrator@2.0.so
 lib64/vendor.samsung.hardware.vibrator@2.1.so
 lib64/vendor.samsung.hardware.vibrator@2.2.so
-"
-for blob in $BLOBS_LIST
-do
-    DELETE_FROM_WORK_DIR "vendor" "$blob"
-done
-
-BLOBS_LIST="
 bin/hw/vendor.samsung.hardware.biometrics.face@2.0-service
 etc/init/vendor.samsung.hardware.biometrics.face@2.0-service.rc
-"
-for blob in $BLOBS_LIST
-do
-    DELETE_FROM_WORK_DIR "vendor" "$blob"
-done
-
-BLOBS_LIST="
 bin/hermesd
 etc/init/hermesd.rc
 "
