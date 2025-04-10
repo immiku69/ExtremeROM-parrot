495770fd51d68d0b5f46693cd381aea04023fd3b
ExtremeXT <75576145+ExtremeXT@users.noreply.github.com>
2025-04-10 20:02:57 +0300

    unica: patches: exynosation: fixup AGAIN
    Signed-off-by: ExtremeXT <75576145+ExtremeXT@users.noreply.github.com>

--- unica/patches/_exynosation/customize.sh
+++ unica/patches/_exynosation/customize.sh
@@ -223,8 +223,11 @@ if [[ $TARGET_SINGLE_SYSTEM_IMAGE == "essi" || $TARGET_SINGLE_SYSTEM_IMAGE == "e
     ADD_TO_WORK_DIR "e2sxxx" "odm" "etc/selinux" 0 0 644 "u:object_r:system_file:s0"
 
     # Create TEEgrisTUIService symlink
-    ln -sf "/system/lib64/libtui_service_jni.so" "$WORK_DIR/system/system/app/TEEgrisTuiService/TEEgrisTuiService/lib/arm64/libtui_service_jni.so"
-    SET_METADATA "system" "system/app/TEEgrisTuiService/TEEgrisTuiService/lib/arm64/libtui_service_jni.so" 0 0 644 "u:object_r:system_file:s0"
+    mkdir -p "$WORK_DIR/system/system/app/TEEgrisTuiService/lib/arm64"
+    ln -sf "/system/lib64/libtui_service_jni.so" "$WORK_DIR/system/system/app/TEEgrisTuiService/lib/arm64/libtui_service_jni.so"
+    SET_METADATA "system" "system/app/TEEgrisTuiService/lib" 0 0 755 "u:object_r:system_file:s0"
+    SET_METADATA "system" "system/app/TEEgrisTuiService/lib/arm64" 0 0 755 "u:object_r:system_file:s0"
+    SET_METADATA "system" "system/app/TEEgrisTuiService/lib/arm64/libtui_service_jni.so" 0 0 644 "u:object_r:system_file:s0"
 
     # Remove Qualcomm Props
     SET_PROP "system" "rild.libpath" --delete
