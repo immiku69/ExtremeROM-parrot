fedb206913585f9950c7f8216f372b4bb693c9bd
ExtremeXT <75576145+ExtremeXT@users.noreply.github.com>
2025-04-12 14:47:09 +0300

    target: c2s: hopefully fix UWB removal now
    Signed-off-by: ExtremeXT <75576145+ExtremeXT@users.noreply.github.com>

--- target/c2s/patches/uwb/customize.sh
+++ target/c2s/patches/uwb/customize.sh
@@ -5,6 +5,8 @@ bin/hw/vendor.samsung.hardware.uwb@1.0-service
 etc/uwb_key
 etc/libuwb-nxp.conf
 etc/libuwb-uci.conf
+etc/permissions/samsung.hardware.uwb.xml
+etc/permissions/android.hardware.uwb.xml
 etc/init/init.vendor.uwb.rc
 etc/init/nxp-uwb-service.rc
 etc/vintf/manifest/uwb-service.xml
@@ -13,3 +15,5 @@ for blob in $BLOBS_LIST
 do
     DELETE_FROM_WORK_DIR "vendor" "$blob"
 done
+
+sed -i -e "/sr100/d" -e "/UWB/d" "$WORK_DIR/vendor/etc/init/init.exynos990.rc"
