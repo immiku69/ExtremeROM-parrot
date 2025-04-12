56658a2e45e5fc13806676a26660de96238c690f
ExtremeXT <75576145+ExtremeXT@users.noreply.github.com>
2025-04-12 16:44:29 +0300

    target: init r8s support
    Signed-off-by: ExtremeXT <75576145+ExtremeXT@users.noreply.github.com>

---
+++ target/r8s/config.sh
@@ -0,0 +1,59 @@
+#
+# Copyright (C) 2024 BlackMesa123
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+
+# Device configuration file for Galaxy S20 FE (Exynos) (r8s)
+TARGET_NAME="Galaxy S20 FE (Exynos)"
+TARGET_CODENAME="r8s"
+TARGET_PLATFORM="exynos990"
+TARGET_FIRMWARE="SM-G780F/AUT/351387292049715"
+TARGET_EXTRA_FIRMWARES=("")
+TARGET_API_LEVEL=33
+TARGET_PRODUCT_FIRST_API_LEVEL=29
+TARGET_VNDK_VERSION=30
+TARGET_SINGLE_SYSTEM_IMAGE="essi"
+TARGET_OS_FILE_SYSTEM="ext4"
+TARGET_SUPER_PARTITION_SIZE=9437184000
+TARGET_SUPER_GROUP_NAME="group_basic"
+TARGET_SUPER_GROUP_SIZE=9432989696
+TARGET_HAS_SYSTEM_EXT=false
+TARGET_INSTALL_METHOD=zip
+TARGET_BOOT_DEVICE_PATH="/dev/block/by-name"
+
+# SEC Product Feature
+TARGET_AUDIO_SUPPORT_ACH_RINGTONE=true
+TARGET_AUDIO_SUPPORT_DUAL_SPEAKER=true
+TARGET_AUDIO_SUPPORT_VIRTUAL_VIBRATION=true
+TARGET_AUTO_BRIGHTNESS_TYPE="3"
+TARGET_DVFS_CONFIG_NAME="dvfs_policy_default"
+TARGET_ESE_CHIP_VENDOR="SLSI"
+TARGET_ESE_COS_NAME="JCOP7.0U"
+TARGET_FP_SENSOR_CONFIG="google_touch_display_ultrasonic"
+TARGET_HAS_HW_MDNIE=true
+TARGET_HAS_MASS_CAMERA_APP=false
+TARGET_HAS_QHD_DISPLAY=true
+TARGET_HFR_MODE="1"
+TARGET_HFR_SUPPORTED_REFRESH_RATE="60,96,48,120"
+TARGET_HFR_DEFAULT_REFRESH_RATE="120"
+TARGET_DISPLAY_CUTOUT_TYPE="center"
+TARGET_HFR_SEAMLESS_BRT=""
+TARGET_HFR_SEAMLESS_LUX=""
+TARGET_IS_ESIM_SUPPORTED=true
+TARGET_MDNIE_SUPPORTED_MODES="65303"
+TARGET_MDNIE_WEAKNESS_SOLUTION_FUNCTION="3"
+TARGET_MULTI_MIC_MANAGER_VERSION="08020"
+TARGET_SSRM_CONFIG_NAME="siop_e2s_s5e9945"
+TARGET_SUPPORT_CUTOUT_PROTECTION=false
