d59ac370bc637a8c6c6cc4a1c0bb1d7e711a5f56
Fred <74693315+xfwdrev@users.noreply.github.com>
2025-04-13 12:06:25 +0300

    platform: exynos9820/990: add WFD downgrade patch

--- platform/exynos9820/patches/stock_blobs/customize.sh
+++ platform/exynos9820/patches/stock_blobs/customize.sh
@@ -27,3 +27,6 @@ SET_METADATA "product" "priv-app/HotwordEnrollmentXGoogleExCORTEXM4" 0 0 755 "u:
 SET_METADATA "product" "priv-app/HotwordEnrollmentXGoogleExCORTEXM4/HotwordEnrollmentXGoogleExCORTEXM4.apk" 0 0 644 "u:object_r:system_file:s0"
 SET_METADATA "product" "priv-app/HotwordEnrollmentXGoogleExCORTEXM4/HotwordEnrollmentXGoogleExCORTEXM4.apk.prof" 0 0 644 "u:object_r:system_file:s0"
 
+echo "Add 32-Bit WFD blobs"
+ADD_TO_WORK_DIR "p3sxxx" "system" "system/bin/remotedisplay" 0 2000 755 "u:object_r:remotedisplay_exec:s0"
+ADD_TO_WORK_DIR "p3sxxx" "system" "system/lib" 0 0 644 "u:object_r:system_lib_file:s0"
