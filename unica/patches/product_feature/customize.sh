279334bf258c0933fea2f8d1afae79d5b15c3f2d
ExtremeXT <75576145+ExtremeXT@users.noreply.github.com>
2025-04-19 17:36:21 +0300

    unica: patches: pf: fix Optical FOD patch
    Signed-off-by: ExtremeXT <75576145+ExtremeXT@users.noreply.github.com>

--- unica/patches/product_feature/customize.sh
+++ unica/patches/product_feature/customize.sh
@@ -111,8 +111,7 @@ if [[ "$(GET_FP_SENSOR_TYPE "$SOURCE_FP_SENSOR_CONFIG")" != "$(GET_FP_SENSOR_TYP
 
     # TODO: handle ultrasonic devices
     if [[ "$(GET_FP_SENSOR_TYPE "$TARGET_FP_SENSOR_CONFIG")" == "optical" ]]; then
-        ADD_TO_WORK_DIR "a56xxx" "system" "."
-        DELETE_FROM_WORK_DIR "system" "system/priv-app/BiometricSetting/oat"
+        ADD_TO_WORK_DIR "r11sxxx" "system" "."
     elif [[ "$(GET_FP_SENSOR_TYPE "$TARGET_FP_SENSOR_CONFIG")" == "side" ]]; then
         ADD_TO_WORK_DIR "a26xxx" "system" "."
         DELETE_FROM_WORK_DIR "system" "system/priv-app/BiometricSetting/oat"
