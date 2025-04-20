73f8c79baa91ab611e96e5025bc77c149b2bca80
ExtremeXT <75576145+ExtremeXT@users.noreply.github.com>
2025-04-20 21:46:55 +0300

    unica: mods: csc: add netowrk speed patch
    Signed-off-by: ExtremeXT <75576145+ExtremeXT@users.noreply.github.com>

--- unica/mods/csc/customize.sh
+++ unica/mods/csc/customize.sh
@@ -18,3 +18,19 @@ find "$WORK_DIR/optics" -type f -name "cscfeature.xml" | while read -r file; do
     " "$file"
     done <<< "$CSCFEATURES"
 done
+
+echo "Patching APKs for network speed monitoring..."
+
+DECODE_APK "system/priv-app/SecSettings/SecSettings.apk"
+DECODE_APK "system_ext/priv-app/SystemUI/SystemUI.apk"
+
+FTP="
+system/priv-app/SecSettings/SecSettings.apk/smali_classes4/com/samsung/android/settings/eternal/provider/items/NotificationsItem.smali
+system/priv-app/SecSettings/SecSettings.apk/smali_classes4/com/samsung/android/settings/notification/ConfigureNotificationMoreSettings$1.smali
+system/priv-app/SecSettings/SecSettings.apk/smali_classes4/com/samsung/android/settings/notification/StatusBarNetworkSpeedController.smali
+system_ext/priv-app/SystemUI/SystemUI.apk/smali/com/android/systemui/Rune.smali
+system_ext/priv-app/SystemUI/SystemUI.apk/smali/com/android/systemui/QpRune.smali
+"
+for f in $FTP; do
+    sed -i "s/CscFeature_Common_SupportZProjectFunctionInGlobal/CscFeature_Setting_SupportRealTimeNetworkSpeed/g" "$APKTOOL_DIR/$f"
+done
