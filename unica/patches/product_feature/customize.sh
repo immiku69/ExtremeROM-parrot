70815da2d5d7f3abcea3a44a0b2a38afd6abebd4
ExtremeXT <75576145+ExtremeXT@users.noreply.github.com>
2025-04-22 23:08:41 +0300

    unica: patches: pf: add patches to disable FP_FEATURE_SENSOR_IS_ULTRASONIC and FP_FEATURE_SENSOR_IS_IN_DISPLAY_TYPE
    Signed-off-by: ExtremeXT <75576145+ExtremeXT@users.noreply.github.com>

--- unica/patches/product_feature/customize.sh
+++ unica/patches/product_feature/customize.sh
@@ -92,9 +92,12 @@ if [[ "$(GET_FP_SENSOR_TYPE "$SOURCE_FP_SENSOR_CONFIG")" != "$(GET_FP_SENSOR_TYP
 
     if [[ "$(GET_FP_SENSOR_TYPE "$TARGET_FP_SENSOR_CONFIG")" == "optical" ]]; then
         ADD_TO_WORK_DIR "r11sxxx" "system" "."
+        APPLY_PATCH "system/framework/services.jar" "fingerprint/services.jar/0001-Set-FP_FEATURE_SENSOR_IS_ULTRASONIC-to-false.patch"
     elif [[ "$(GET_FP_SENSOR_TYPE "$TARGET_FP_SENSOR_CONFIG")" == "side" ]]; then
         ADD_TO_WORK_DIR "b6qxxx" "system" "."
         DELETE_FROM_WORK_DIR "system" "system/priv-app/BiometricSetting/oat"
+        APPLY_PATCH "system/framework/services.jar" "fingerprint/services.jar/0001-Set-FP_FEATURE_SENSOR_IS_ULTRASONIC-to-false.patch"
+        APPLY_PATCH "system/framework/services.jar" "fingerprint/services.jar/0002-Set-FP_FEATURE_SENSOR_IS_IN_DISPLAY_TYPE-to-false.patch"
     fi
 fi
 
