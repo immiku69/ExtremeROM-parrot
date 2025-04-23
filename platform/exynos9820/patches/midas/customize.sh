6526f0bdbcb38806312156dfa1e74006539111c2
ExtremeXT <75576145+ExtremeXT@users.noreply.github.com>
2025-04-24 00:52:07 +0300

    platform: exynos9820/990: midas: use p3s midas libraries
    * Thanks to @ipoij8
    
    Signed-off-by: ExtremeXT <75576145+ExtremeXT@users.noreply.github.com>

--- platform/exynos9820/patches/midas/customize.sh
+++ platform/exynos9820/patches/midas/customize.sh
@@ -11,3 +11,7 @@ sed -i "s/p3s/$SOURCE_CODENAME/g" "$WORK_DIR/vendor/etc/midas/midas_config.json"
 # Add S21 Photo Remaster Service
 DELETE_FROM_WORK_DIR "system" "system/priv-app/PhotoRemasterService/oat"
 ADD_TO_WORK_DIR "p3sxxx" "system" "system/priv-app/PhotoRemasterService/PhotoRemasterService.apk"
+
+# Add S21 MIDAS libraries
+ADD_TO_WORK_DIR "p3sxxx" "system" "system/lib64/libmidas_core.camera.samsung.so"
+ADD_TO_WORK_DIR "p3sxxx" "system" "system/lib64/libmidas_DNNInterface.camera.samsung.so"
