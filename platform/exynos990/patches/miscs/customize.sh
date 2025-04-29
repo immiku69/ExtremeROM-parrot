54589c8ef951ae223670f1fc57cdbce0bf2c4917
ExtremeXT <75576145+ExtremeXT@users.noreply.github.com>
2025-04-29 21:14:55 +0300

    target: x1s/y2s/z3s/r8s: enable Adaptive HFR
    Signed-off-by: ExtremeXT <75576145+ExtremeXT@users.noreply.github.com>

--- platform/exynos990/patches/miscs/customize.sh
+++ platform/exynos990/patches/miscs/customize.sh
@@ -15,6 +15,16 @@ SET_PROP "vendor" "debug.sf.latch_unsignaled" "1"
 SET_PROP "vendor" "debug.sf.high_fps_late_app_phase_offset_ns" "0"
 SET_PROP "vendor" "debug.sf.high_fps_late_sf_phase_offset_ns" "0"
 
+echo "Setting Adaptive HFR flags"
+if [[ "$MODEL" != "c1s" && "$MODEL" != "c2s" ]]; then
+    SET_PROP "vendor" "debug.sf.show_refresh_rate_overlay_render_rate" "true"
+    SET_PROP "vendor" "ro.surface_flinger.game_default_frame_rate_override" "60"
+    SET_PROP "vendor" "ro.surface_flinger.use_content_detection_for_refresh_rate" "true"
+    SET_PROP "vendor" "ro.surface_flinger.set_touch_timer_ms" "300"
+    SET_PROP "vendor" "ro.surface_flinger.set_idle_timer_ms" "600"
+    SET_PROP "vendor" "ro.surface_flinger.enable_frame_rate_override" "true"
+fi
+
 echo "Disabling encryption"
 # Encryption
 LINE=$(sed -n "/^\/dev\/block\/by-name\/userdata/=" "$WORK_DIR/vendor/etc/fstab.exynos990")
