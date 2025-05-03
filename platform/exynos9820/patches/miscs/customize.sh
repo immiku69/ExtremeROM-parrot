34e8172ae3bd6b64302bd00e593b2db1514e2299
=?UTF-8?q?duha=C5=84?= <duhan2002@gmail.com>
2025-05-03 14:59:04 +0300

    platform: exynos9820/990: fix Device Control
    Signed-off-by: ExtremeXT <75576145+ExtremeXT@users.noreply.github.com>

--- platform/exynos9820/patches/miscs/customize.sh
+++ platform/exynos9820/patches/miscs/customize.sh
@@ -9,7 +9,6 @@ sed -i -e "/ODE/d" -e "/keydata/d" -e "/keyrefuge/d" "$WORK_DIR/vendor/etc/fstab
 echo "Enabling updateable APEX images"
 SET_PROP "vendor" "ro.apex.updatable" "true"
 
-
 echo "Setting /data to F2FS"
 FROM="noatime,nosuid,nodev,noauto_da_alloc,discard,journal_checksum,data=ordered,errors=panic"
 TO="noatime,nosuid,nodev,discard,usrquota,grpquota,fsync_mode=nobarrier,reserve_root=32768,resgid=5678"
@@ -26,3 +25,15 @@ SET_PROP "vendor" "debug.sf.high_fps_late_sf_phase_offset_ns" "0"
 echo "Disabling HFR"
 SET_PROP "vendor" "ro.surface_flinger.enable_frame_rate_override" "false"
 SET_PROP "vendor" "ro.surface_flinger.use_content_detection_for_refresh_rate" "false"
+
+# For some reason we are missing 2 permissions here: android.hardware.security.model.compatible and android.software.controls
+# First one is related to encryption and second one to SmartThings Device Control
+echo "Patching vendor permissions"
+sed -i '$d' "$WORK_DIR/vendor/etc/permissions/handheld_core_hardware.xml"
+echo >> "$WORK_DIR/vendor/etc/permissions/handheld_core_hardware.xml"
+echo "    <!-- Indicate support for the Android security model per the CDD. -->" >> "$WORK_DIR/vendor/etc/permissions/handheld_core_hardware.xml"
+echo "    <feature name=\"android.hardware.security.model.compatible\"/>" >> "$WORK_DIR/vendor/etc/permissions/handheld_core_hardware.xml"
+echo >> "$WORK_DIR/vendor/etc/permissions/handheld_core_hardware.xml"
+echo "    <!--  Feature to specify if the device supports controls.  -->" >> "$WORK_DIR/vendor/etc/permissions/handheld_core_hardware.xml"
+echo "    <feature name=\"android.software.controls\"/>" >> "$WORK_DIR/vendor/etc/permissions/handheld_core_hardware.xml"
+echo "</permissions>" >> "$WORK_DIR/vendor/etc/permissions/handheld_core_hardware.xml"
