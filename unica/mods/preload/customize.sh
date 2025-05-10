666c31d45280bb9e24c09246b07743f31ca40052
ocin4ever <85343782+Ocin4ever@users.noreply.github.com>
2025-05-10 23:13:31 +0200

    unica: mods: import preload from unica

---
+++ unica/mods/preload/customize.sh
@@ -0,0 +1,35 @@
+# Samsung Internet Browser
+DOWNLOAD_FILE "$(GET_GALAXY_STORE_DOWNLOAD_URL "com.sec.android.app.sbrowser")" \
+    "$WORK_DIR/system/system/preload/SBrowser/SBrowser.apk"
+
+# Samsung Notes
+DOWNLOAD_FILE "$(GET_GALAXY_STORE_DOWNLOAD_URL "com.samsung.android.app.notes")" \
+    "$WORK_DIR/system/system/preload/Notes40_Removable/Notes40_Removable.apk"
+
+# Samsung Calculator
+DOWNLOAD_FILE "$(GET_GALAXY_STORE_DOWNLOAD_URL "com.sec.android.app.popupcalculator")" \
+    "$WORK_DIR/system/system/preload/SecCalculator_R/SecCalculator_R.apk"
+
+# Samsung Clock
+DOWNLOAD_FILE "$(GET_GALAXY_STORE_DOWNLOAD_URL "com.sec.android.app.clockpackage")" \
+    "$WORK_DIR/system/system/preload/ClockPackage/ClockPackage.apk"
+
+# Samsung Voice Recorder
+DOWNLOAD_FILE "$(GET_GALAXY_STORE_DOWNLOAD_URL "com.sec.android.app.voicenote")" \
+    "$WORK_DIR/system/system/preload/VoiceNote_5.0/VoiceNote_5.0.apk"
+
+sed -i "/system\/preload/d" "$WORK_DIR/configs/fs_config-system" \
+    && sed -i "/system\/preload/d" "$WORK_DIR/configs/file_context-system"
+while read -r i; do
+    FILE="$(echo -n "$i"| sed "s.$WORK_DIR/system/..")"
+    [ -d "$i" ] && echo "$FILE 0 0 755 capabilities=0x0" >> "$WORK_DIR/configs/fs_config-system"
+    [ -f "$i" ] && echo "$FILE 0 0 644 capabilities=0x0" >> "$WORK_DIR/configs/fs_config-system"
+    FILE="$(echo -n "$FILE" | sed 's/\./\\./g')"
+    echo "/$FILE u:object_r:system_file:s0" >> "$WORK_DIR/configs/file_context-system"
+done <<< "$(find "$WORK_DIR/system/system/preload")"
+
+rm -f "$WORK_DIR/system/system/etc/vpl_apks_count_list.txt"
+while read -r i; do
+    FILE="$(echo "$i" | sed "s.$WORK_DIR/system..")"
+    echo "$FILE" >> "$WORK_DIR/system/system/etc/vpl_apks_count_list.txt"
+done <<< "$(find "$WORK_DIR/system/system/preload" -name "*.apk" | sort)"
