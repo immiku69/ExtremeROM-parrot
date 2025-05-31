b3a3247297b6418241581b0df524dde6c6cc2fd3
ExtremeXT <75576145+ExtremeXT@users.noreply.github.com>
2025-05-31 14:50:03 +0300

    unica: mods: settings: disable ASKS
    Signed-off-by: ExtremeXT <75576145+ExtremeXT@users.noreply.github.com>

--- unica/mods/settings/customize.sh
+++ unica/mods/settings/customize.sh
@@ -14,3 +14,7 @@ done
 echo "Adding Multi-User Support"
 SET_PROP "system" "fw.max_users" "8"
 SET_PROP "system" "fw.show_multiuserui" "1"
+
+# ro.build.2ndbrand is always "false"
+echo "Disabling ASKS"
+sed -i "s/ro.build.official.release/ro.build.2ndbrand/g" "$APKTOOL_DIR/system/framework/services.jar/smali/com/android/server/asks/ASKSManagerService.smali"
