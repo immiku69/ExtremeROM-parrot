669bc387e9f71b721e9b4284e6b8cbd95aaa3a93
ExtremeXT <75576145+ExtremeXT@users.noreply.github.com>
2025-06-04 21:53:56 +0300

    unica: mods: settings: move cached app freezer here
    Signed-off-by: ExtremeXT <75576145+ExtremeXT@users.noreply.github.com>

--- unica/mods/settings/customize.sh
+++ unica/mods/settings/customize.sh
@@ -15,6 +15,9 @@ echo "Adding Multi-User Support"
 SET_PROP "system" "fw.max_users" "8"
 SET_PROP "system" "fw.show_multiuserui" "1"
 
+echo "Enabling Cached App Freezer"
+SET_PROP "system" "persist.device_config.activity_manager_native_boot.use_freezer" "true"
+
 # ro.build.2ndbrand is always "false"
 echo "Disabling ASKS"
 sed -i "s/ro.build.official.release/ro.build.2ndbrand/g" "$APKTOOL_DIR/system/framework/services.jar/smali/com/android/server/asks/ASKSManagerService.smali"
