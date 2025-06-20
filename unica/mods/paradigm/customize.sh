e815b369b61ac6d05db82ccff21b55985b1997a4
Ocin4ever <85343782+Ocin4ever@users.noreply.github.com>
2025-06-20 19:09:02 +0300

    unica: patches: spen: reverse for r12s; unica: mods: add paradigm
    Signed-off-by: ExtremeXT <75576145+ExtremeXT@users.noreply.github.com>

---
+++ unica/mods/paradigm/customize.sh
@@ -0,0 +1,4 @@
+# Now Brief
+ADD_TO_WORK_DIR "pa3qxxx" "system" "system/priv-app/SamsungSmartSuggestions/SamsungSmartSuggestions.apk"
+ADD_TO_WORK_DIR "pa3qxxx" "system" "system/priv-app/SamsungSmartSuggestions/oat"
+SET_FLOATING_FEATURE_CONFIG "SEC_FLOATING_FEATURE_FRAMEWORK_SUPPORT_PERSONALIZED_DATA_CORE" "TRUE"
