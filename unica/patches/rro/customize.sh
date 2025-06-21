3ee9679caf0396aaac9863fc7c5e877d339bfda3
ExtremeXT <75576145+ExtremeXT@users.noreply.github.com>
2025-06-21 16:44:11 +0300

    unica: patches: rro: adapt to r12s
    Signed-off-by: ExtremeXT <75576145+ExtremeXT@users.noreply.github.com>

--- unica/patches/rro/customize.sh
+++ unica/patches/rro/customize.sh
@@ -1,11 +1,11 @@
 if [[ -d "$SRC_DIR/target/$TARGET_CODENAME/overlay" ]]; then
-    DECODE_APK "/product/overlay/framework-res__pa3qxxx__auto_generated_rro_product.apk"
+    DECODE_APK "/product/overlay/framework-res__r12sxxx__auto_generated_rro_product.apk"
 
     echo "Applying stock overlay configs"
-    rm -rf "$APKTOOL_DIR/product/overlay/framework-res__pa3qxxx__auto_generated_rro_product.apk/res"
+    rm -rf "$APKTOOL_DIR/product/overlay/framework-res__r12sxxx__auto_generated_rro_product.apk/res"
     cp -a --preserve=all \
         "$SRC_DIR/target/$TARGET_CODENAME/overlay" \
-        "$APKTOOL_DIR/product/overlay/framework-res__pa3qxxx__auto_generated_rro_product.apk/res"
+        "$APKTOOL_DIR/product/overlay/framework-res__r12sxxx__auto_generated_rro_product.apk/res"
 fi
 
 # TODO: Add a proper check if we need to remove this
