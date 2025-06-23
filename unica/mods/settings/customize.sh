5bea5f109ae46f7b1718b6ac6b715290c2e5414b
ExtremeXT <75576145+ExtremeXT@users.noreply.github.com>
2025-06-23 18:59:05 +0300

    unica: mods: settings: fix model number
    Signed-off-by: ExtremeXT <75576145+ExtremeXT@users.noreply.github.com>

--- unica/mods/settings/customize.sh
+++ unica/mods/settings/customize.sh
@@ -8,7 +8,7 @@ system/priv-app/SecSettings/SecSettings.apk/smali_classes4/com/samsung/android/s
 system/priv-app/SecSettings/SecSettings.apk/smali_classes4/com/samsung/android/settings/deviceinfo/batteryinfo/SecBatteryInfoFragment.smali
 "
 for f in $FTP; do
-    sed -i "s/SM-A236B/SM-S938B/g" "$APKTOOL_DIR/$f"
+    sed -i "s/SM-A236B/SM-S721B/g" "$APKTOOL_DIR/$f"
 done
 
 echo "Adding Multi-User Support"
