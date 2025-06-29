745a6dc5a345111af89b38fc4fbdb3ad4e87d225
dupazlasu <dupazlasu66@gmail.com>
2025-06-29 23:44:05 +0200

    unica: patches: product_feature: bring back seamless BRT and LUX patching
    Co-authored-by: ExtremeXT <75576145+ExtremeXT@users.noreply.github.com>
    Signed-off-by: dupazlasu <dupazlasu66@gmail.com>

--- unica/patches/product_feature/customize.sh
+++ unica/patches/product_feature/customize.sh
@@ -168,7 +168,20 @@ if [[ "$SOURCE_MDNIE_SUPPORTED_MODES" != "$TARGET_MDNIE_SUPPORTED_MODES" ]]; the
     done
 fi
 
-APPLY_PATCH "system/framework/framework.jar" "hfr/framework.jar/0001-Remove-brightness-threshold-values.patch"
+DECODE_APK "system/framework/framework.jar"
+    
+if [[ "$TARGET_HFR_SEAMLESS_BRT" == "none" && "$TARGET_HFR_SEAMLESS_LUX" == "none" ]]; then
+    APPLY_PATCH "system/framework/framework.jar" "hfr/framework.jar/0001-Remove-brightness-threshold-values.patch"
+else
+
+    FTP="
+    system/framework/framework.jar/smali_classes6/com/samsung/android/hardware/display/RefreshRateConfig.smali
+    "
+    for f in $FTP; do
+        sed -i "s/\"$SOURCE_HFR_SEAMLESS_BRT\"/\"$TARGET_HFR_SEAMLESS_BRT\"/g" "$APKTOOL_DIR/$f"
+        sed -i "s/\"$SOURCE_HFR_SEAMLESS_LUX\"/\"$TARGET_HFR_SEAMLESS_LUX\"/g" "$APKTOOL_DIR/$f"
+    done
+fi
 
 if [[ "$SOURCE_HFR_MODE" != "$TARGET_HFR_MODE" ]]; then
     echo "Applying HFR_MODE patches"
