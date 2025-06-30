265f21c088edb98d125cb493e537ca0127e394f5
xfwdrev <74693315+xfwdrev@users.noreply.github.com>
2025-06-30 19:29:09 +0200

    platform: exynos2200: stock_blobs: nuke libsoundboostereq_legacy.so and lib_SAG_EQ_ver2000.so * follow up of 5021c09 and 0ec3430
    Signed-off-by: dupazlasu <dupazlasu66@gmail.com>

--- platform/exynos2200/patches/stock_blobs/customize.sh
+++ platform/exynos2200/patches/stock_blobs/customize.sh
@@ -2,6 +2,8 @@
 # S22 Series -> SoundBooster 1100
 echo "Replacing SoundBooster"
 DELETE_FROM_WORK_DIR "system" "system/lib64/lib_SoundBooster_ver2000.so"
+DELETE_FROM_WORK_DIR "system" "system/lib64/lib_SAG_EQ_ver2000.so"
+DELETE_FROM_WORK_DIR "system" "system/lib64/libsoundboostereq_legacy.so"
 ADD_TO_WORK_DIR "$TARGET_FIRMWARE" "system" "system/lib64/lib_SoundBooster_ver1100.so" 0 0 644 "u:object_r:system_lib_file:s0"
 ADD_TO_WORK_DIR "$TARGET_FIRMWARE" "system" "system/lib64/libsamsungSoundbooster_plus_legacy.so" 0 0 644 "u:object_r:system_lib_file:s0"
 
