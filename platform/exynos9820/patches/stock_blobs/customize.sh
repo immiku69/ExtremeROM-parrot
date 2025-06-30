94a3c7db5d1deda22eee09f413253af5bce304ef
xfwdrev <74693315+xfwdrev@users.noreply.github.com>
2025-06-30 22:46:57 +0800

    platform: 9820/990/2100: stock_blobs: Delete libsoundboostereq_legacy.so
    Also related to commit 0ec3430
    
    It spams the log too because it can't find that specific library.
    
    Signed-off-by: xfwdrev <74693315+xfwdrev@users.noreply.github.com>

--- platform/exynos9820/patches/stock_blobs/customize.sh
+++ platform/exynos9820/patches/stock_blobs/customize.sh
@@ -1,10 +1,11 @@
 SKIPUNZIP=1
 
-# S25 Ultra OneUI 7 -> SoundBooster 2000
+# S24 FE OneUI 7 -> SoundBooster 2000
 # S10 Series -> SoundBooster 1000
 echo "Replacing SoundBooster"
 DELETE_FROM_WORK_DIR "system" "system/lib64/lib_SoundBooster_ver2000.so"
 DELETE_FROM_WORK_DIR "system" "system/lib64/lib_SAG_EQ_ver2000.so"
+DELETE_FROM_WORK_DIR "system" "system/lib64/libsoundboostereq_legacy.so"
 ADD_TO_WORK_DIR "$TARGET_FIRMWARE" "system" "system/lib64/lib_SoundBooster_ver1000.so" 0 0 644 "u:object_r:system_lib_file:s0"
 ADD_TO_WORK_DIR "$TARGET_FIRMWARE" "system" "system/lib64/libsamsungSoundbooster_plus_legacy.so" 0 0 644 "u:object_r:system_lib_file:s0"
 
