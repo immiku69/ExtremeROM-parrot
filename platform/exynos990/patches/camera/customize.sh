04e8a18393db9072cc699ecffb12431b28ff6e03
ExtremeXT <75576145+ExtremeXT@users.noreply.github.com>
2025-07-03 13:02:51 +0300

    platform: exynos990: patches: camera: fix typo
    * How did I even miss this
    
    Signed-off-by: ExtremeXT <75576145+ExtremeXT@users.noreply.github.com>

--- platform/exynos990/patches/camera/customize.sh
+++ platform/exynos990/patches/camera/customize.sh
@@ -33,7 +33,7 @@ do
     ADD_TO_WORK_DIR "$TARGET_FIRMWARE" "system" "$blob" 0 0 644 "u:object_r:system_lib_file:s0"
 done
 
-if [[ "$MODEL" == "c1s" || "$MODEL" == "c2s" ]]; then
+if [[ "$TARGET_CODENAME" == "c1s" || "$TARGET_CODENAME" == "c2s" ]]; then
     BLOBS_LIST="
     system/lib64/libofi_seva.so
     system/lib64/libofi_klm.so
