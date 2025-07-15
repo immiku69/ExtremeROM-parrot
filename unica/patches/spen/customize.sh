0b1f650425219f29b0e3ab2efa600a259193ffda
ExtremeXT <75576145+ExtremeXT@users.noreply.github.com>
2025-07-15 23:23:59 +0300

    unica: patches: spen: fixup
    Signed-off-by: ExtremeXT <75576145+ExtremeXT@users.noreply.github.com>

--- unica/patches/spen/customize.sh
+++ unica/patches/spen/customize.sh
@@ -3,8 +3,8 @@ REGION=$(echo -n "$TARGET_FIRMWARE" | cut -d "/" -f 2)
 
 if [ -d "$FW_DIR/${MODEL}_${REGION}/system/system/media/audio/pensounds" ]; then
     echo "Adding SPen stack"
-    ADD_TO_WORK_DIR "pa3qxxx" "system" "system/app/AirGlance"
-    ADD_TO_WORK_DIR "pa3qxxx" "system" "system/app/LiveDrawing"
+    ADD_TO_WORK_DIR "dm3qxxx" "system" "system/app/AirGlance"
+    ADD_TO_WORK_DIR "dm3qxxx" "system" "system/app/LiveDrawing"
     ADD_TO_WORK_DIR "pa3qxxx" "system" "system/etc/default-permissions/default-permissions-com.samsung.android.service.aircommand.xml"
     ADD_TO_WORK_DIR "pa3qxxx" "system" "system/etc/permissions/com.samsung.android.spensdk.framework-v1.xml"
     ADD_TO_WORK_DIR "pa3qxxx" "system" "system/etc/permissions/privapp-permissions-com.samsung.android.app.readingglass.xml"
