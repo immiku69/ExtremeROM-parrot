58be935e1ff28cb42e3ed663a57e925f5c8bb899
dupazlasu <dupazlasu66@gmail.com>
2025-07-16 09:18:36 +0200

    platform: exynos2200: extremekrnl: fixup
    Signed-off-by: dupazlasu <dupazlasu66@gmail.com>

--- platform/exynos2200/patches/extremekrnl/customize.sh
+++ platform/exynos2200/patches/extremekrnl/customize.sh
@@ -112,9 +112,9 @@ UPDATE_MODULES()
     mv -v "$KERNEL_TMP_DIR-$TARGET_PLATFORM/build/out/$TARGET_CODENAME/modules_dlkm/fingerprint_sysfs.ko" "$WORK_DIR/vendor_dlkm/lib/modules"
     mv -v "$KERNEL_TMP_DIR-$TARGET_PLATFORM/build/out/$TARGET_CODENAME/modules_dlkm/input_booster_lkm.ko" "$WORK_DIR/vendor_dlkm/lib/modules"
     mv -v "$KERNEL_TMP_DIR-$TARGET_PLATFORM/build/out/$TARGET_CODENAME/modules_dlkm/sec_debug_coredump.ko" "$WORK_DIR/vendor_dlkm/lib/modules"
-    if [[ "$TARGET_CODENAME" == "r0s" || "$TARGET_CODENAME" == "g0s" ]]; then
+    if [[ "$TARGET_CODENAME" == "r0s" ]]; then
          mv -v "$KERNEL_TMP_DIR-$TARGET_PLATFORM/build/out/$TARGET_CODENAME/modules_dlkm/wlan.ko" "$WORK_DIR/vendor_dlkm/lib/modules"
-    elif [[ "$TARGET_CODENAME" == "b0s" ]]; then
+    elif [[ "$TARGET_CODENAME" == "b0s"  || "$TARGET_CODENAME" == "g0s" ]]; then
         mv -v "$KERNEL_TMP_DIR-$TARGET_PLATFORM/build/out/$TARGET_CODENAME/modules_dlkm/dhd.ko" "$WORK_DIR/vendor_dlkm/lib/modules"
     fi
 }
