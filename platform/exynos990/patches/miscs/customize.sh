b18c48258ecb51ba2d8cbf65cf2b77fc2437745d
ExtremeXT <75576145+ExtremeXT@users.noreply.github.com>
2025-07-22 01:14:31 +0300

    platform: exynos990: fixup TARGET_CODENAME
    * How tf did I even mess this up so badly
    
    Signed-off-by: ExtremeXT <75576145+ExtremeXT@users.noreply.github.com>

--- platform/exynos990/patches/miscs/customize.sh
+++ platform/exynos990/patches/miscs/customize.sh
@@ -13,19 +13,19 @@ SET_PROP "vendor" "debug.sf.high_fps_late_app_phase_offset_ns" "0"
 SET_PROP "vendor" "debug.sf.high_fps_late_sf_phase_offset_ns" "0"
 
 echo "Setting Adaptive HFR flags"
-if [[ "$MODEL" != "c1s" && "$MODEL" != "c2s" && "$MODEL" != "r8s" ]]; then
+if [[ "$TARGET_CODENAME" != "c1s" && "$TARGET_CODENAME" != "c2s" && "$TARGET_CODENAME" != "r8s" ]]; then
     SET_PROP "vendor" "debug.sf.show_refresh_rate_overlay_render_rate" "true"
     SET_PROP "vendor" "ro.surface_flinger.game_default_frame_rate_override" "60"
     SET_PROP "vendor" "ro.surface_flinger.use_content_detection_for_refresh_rate" "true"
     SET_PROP "vendor" "ro.surface_flinger.set_touch_timer_ms" "300"
     SET_PROP "vendor" "ro.surface_flinger.set_idle_timer_ms" "600"
     SET_PROP "vendor" "ro.surface_flinger.enable_frame_rate_override" "true"
-elif [[ "$MODEL" == "c1s" || "$MODEL" == "r8s" ]]; then
+elif [[ "$TARGET_CODENAME" == "c1s" || "$TARGET_CODENAME" == "r8s" ]]; then
     SET_PROP "vendor" "debug.sf.show_refresh_rate_overlay_render_rate" "true"
     SET_PROP "vendor" "ro.surface_flinger.game_default_frame_rate_override" "60"
     SET_PROP "vendor" "ro.surface_flinger.use_content_detection_for_refresh_rate" "false"
     SET_PROP "vendor" "ro.surface_flinger.enable_frame_rate_override" "false"
-elif [[ "$MODEL" == "c2s" ]]; then
+elif [[ "$TARGET_CODENAME" == "c2s" ]]; then
     SET_PROP "vendor" "debug.sf.show_refresh_rate_overlay_render_rate" "true"
     SET_PROP "vendor" "ro.surface_flinger.game_default_frame_rate_override" "60"
 fi
